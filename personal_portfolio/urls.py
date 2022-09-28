40e21f6244b013f5aa3b0e4d4e2da1322cea7c2f
doctor-silence <vladimirkrizhanovski@gmail.com>
2022-09-28 15:47:48 +0700

    add_url_portfolio

--- personal_portfolio/urls.py
+++ personal_portfolio/urls.py
@@ -16,12 +16,15 @@ Including another URLconf
 from typing import List, Any
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+from portfolio import views
 
 urlpatterns: list[Any] = [
     path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
+    path('blog/', include('blog.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
